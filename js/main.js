b25586c7869ed0a02d9c4d9927fe5fb1589a7890
Volodymyr <39493622+VolodymyrKozel@users.noreply.github.com>
2023-11-11 13:25:31 +0100

    v6

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// Get the link to top
+let toTop = document.getElementById("to-top");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    toTop.style.display = "flex";
+  } else {
+    toTop.style.display = "none";
+  }
+}
 function closeMobMenu() {
   const closeBtn = document.getElementById("mobile-menu");
   closeBtn.classList.add("is-close");
